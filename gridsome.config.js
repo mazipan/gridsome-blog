1aac73f6490194dd310f3e204a67645437ab1cd8
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-03-15 14:31:41 +0700

    chore: add new article sample

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,12 +1,12 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Gridsome Blog by @mazipan',
+  siteDescription: 'An example to create Gridsome\'s Blog with Gridsome Blog Starter template',
 
   plugins: [
     {
