2d1525c422564697af8b91ae670e3e99265f2a63
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-09-27 21:45:21 +0700

    fix: update core-js

--- pnpmfile.js
+++ pnpmfile.js
@@ -2,7 +2,7 @@ function readPackage(pkg, context) {
   switch(pkg.name) {
     case "gridsome":
       pkg.dependencies = Object.assign({}, pkg.dependencies, {
-        "core-js": "^2.6.9",
+        "core-js": "^3.6.5",
       });
       break;
   }
